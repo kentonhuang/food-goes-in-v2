2e43f471dbf9d765d595e10696e73ee608662d78
Kenton Huang <huangkenton@gmail.com>
2018-06-27 16:28:10 -0700

    header stuff

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,11 @@
 <template>
-	<div class="header">
-		
-	</div>
+	<header class="header">
+		<form class="form">
+			<input type="text" class="form__input form__input--term">
+			<input type="text" class="form__input">
+			<button type="submit">Find Restaurant</button>
+		</form>
+	</header>
 </template>
 
 <script>
@@ -14,5 +18,36 @@ export default {
 	.header {
 		height: 6.4rem;
 		background-color: $color-grey-dark-2;
+		padding: 1.5rem 3rem;
+		display: flex;
+		justify-content: center;
+	}
+
+	.form {
+		position: relative;
+		display: flex;
+
+		&__input {
+			border: none;
+			padding: 1rem 1.5rem;
+			font-size: 1.4rem;
+			width: 50%;
+
+			&:focus {
+				outline: none;
+			}
+
+			&--term::after {
+				height: 100%;
+				width: 1px;
+				content: 'a';
+				background-color: #000;
+			}
+		}
+
+		button {
+			position: absolute;
+		}
+		
 	}
 </style>
