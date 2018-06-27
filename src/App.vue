bc59bea75f6cf13c96d5e41aef4835a74726c23d
Kenton Huang <huangkenton@gmail.com>
2018-06-27 15:02:27 -0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+		<app-header></app-header>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -8,13 +9,19 @@
   </div>
 </template>
 
+<script>
+	import Header from '@/components/Header';
+
+	export default {
+		components: {
+			appHeader: Header
+		}
+	}
+</script>
+
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	text-align: center;
 }
 #nav {
   padding: 30px;
@@ -22,7 +29,7 @@
     font-weight: bold;
     color: #2c3e50;
     &.router-link-exact-active {
-      color: #42b983;
+      color: $color-primary;
     }
   }
 }
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="header">
+		
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+	.header {
+		height: 6.4rem;
+		background-color: $color-grey-dark-2;
+	}
+</style>
